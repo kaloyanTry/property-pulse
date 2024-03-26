9316004f702c0df58d9ef454bff51a76c5ff8b2f
kaloyanTry <kaloyantryavna@gmail.com>
2024-03-26 19:19:23 +0200

    Home Properties List

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -3,7 +3,7 @@ import properties from '@/properties.json';
 
 const PropertiesPage = () => {
   return (
-    <section>
+    <section className='px-4 py-6'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
         {properties.length === 0 ? (
           <p>No properties found</p>
