cc1e30dc7b9b4ce0f57915a5c03f1bd2325405f1
kaloyanTry <kaloyantryavna@gmail.com>
2024-04-28 11:03:15 +0300

    Edited PropertiesPage

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,43 +1,12 @@
-'use client';
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
-import Link from 'next/link';
-import { FaArrowAltCircleLeft } from 'react-icons/fa';
 import PropertyCard from '@/components/PropertyCard';
-import Spinner from '@/components/Spinner';
 import PropertySearchForm from '@/components/PropertySearchForm';
+import { fetchProperties } from '@/utils/requests';
 
-const SearchResultsPage = () => {
-  const searchParams = useSearchParams();
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
 
-  const [properties, setProperties] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const location = searchParams.get('location');
-  const propertyType = searchParams.get('propertyType');
-
-  useEffect(() => {
-    const fetchSearchResults = async () => {
-      try {
-        const res = await fetch(
-          `/api/properties/search?location=${location}&propertyType=${propertyType}`
-        );
-
-        if (res.status === 200) {
-          const data = await res.json();
-          setProperties(data);
-        } else {
-          setProperties([]);
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchSearchResults();
-  }, [location, propertyType]);
+  //Sort properties by date:
+  properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   return (
     <>
@@ -46,18 +15,10 @@ const SearchResultsPage = () => {
           <PropertySearchForm />
         </div>
       </section>
-
       <section className='px-4 py-6'>
         <div className='container-xl lg:container m-auto px-4 py-6'>
-          <Link
-            href='/properties'
-            className='flex items-center text-blue-500 hover:underline mb-3'
-          >
-            <FaArrowAltCircleLeft className='mr-2 mb-1' /> Back To Properties
-          </Link>
-          <h1 className='text-2xl mb-4'>Search Results</h1>
           {properties.length === 0 ? (
-            <p>No search results found</p>
+            <p>No seaarch results found</p>
           ) : (
             <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
               {properties.map((property) => (
@@ -70,4 +31,5 @@ const SearchResultsPage = () => {
     </>
   );
 };
-export default SearchResultsPage;
+
+export default PropertiesPage;
